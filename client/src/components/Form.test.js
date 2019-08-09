cc5ddf0d331f8ea0f64b0b3295806d8ac1431e81
Cory Hedeen <hedeencory@gmail.com>
2019-08-09 13:11:49 -0400

    Test button

--- client/src/components/Form.test.js
+++ client/src/components/Form.test.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import FormikLogin from './Form';
+import LoginForm from './Form';
 import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/react/cleanup-after-each';
 
 describe('<FormikLogin/>', () => {
   it('renders without crashing', () => {
     const login = render(<FormikLogin />)
+  })
+  it('fires', () => {
+    const click = jest.fn();
+    const { getByText } = render(<LoginForm/>)
+    const button = getByText(/^submit$/i);
+    fireEvent.click(button);
+    expect(click).toBeCalled();
   })
 })
