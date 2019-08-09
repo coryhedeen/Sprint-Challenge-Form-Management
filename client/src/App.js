f579f8b348663c41c4fc3ee31043cf19b19c8328
Cory Hedeen <hedeencory@gmail.com>
2019-08-09 11:48:30 -0400

    Render Form

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import FormikLogin from './components/Form';
+
+function App() {
+  return (
+    <div className="App">
+      <FormikLogin/>
+
+    </div>
+  );
+}
+
+export default App;
