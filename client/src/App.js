0d8e0cb37b700019213d2cecb95167d4f885bd32
Cory Hedeen <hedeencory@gmail.com>
2019-08-09 12:51:16 -0400

    Render data from Get request

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,38 @@
 import React from 'react';
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 import FormikLogin from './components/Form';
+import GoodFood from './components/UserCard';
 
-function App() {
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      req: []
+    }
+  }
+
+  componentDidMount(){
+    axios
+    .get("http://localhost:5000/api/restricted/data")
+    .then(res => {this.setState({
+      req: res.data
+    })})
+    .catch(err => console.log(err))
+  }
+
+
+
+
+  render(){
   return (
     <div className="App">
       <FormikLogin/>
-
+      {this.state.req.map(item => <GoodFood card={item}/>)}
     </div>
   );
+}
 }
 
 export default App;
